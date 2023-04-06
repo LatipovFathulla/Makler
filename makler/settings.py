a7e834f80add6329857f288367b2bec6a31dd70f
FathullaLatipov <fathullaxonlatipov@gmail.com>
2023-04-06 16:26:46 +0500

    jwt change

--- makler/settings.py
+++ makler/settings.py
@@ -190,6 +190,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=360),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=360),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
 
