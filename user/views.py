15d0a6f6049a917b6fa5c962ee63a3b54b68a700
FathullaLatipov <fathullaxonlatipov@gmail.com>
2023-07-12 00:31:14 +0500

    bahtibek fixed

--- user/views.py
+++ user/views.py
@@ -84,7 +84,7 @@ def generate_confirmation_code():
 
 def save_confirmation_code(code, phone_number, password):
     # Сохранение кода подтверждения в кэше с указанием версии
-    cache.set(f"confirmation_code_{phone_number}", code, timeout=None, version=str(code))
+    cache.set(f"confirmation_code_{phone_number}", code, version=str(code))
     print(f"Saved confirmation code: {code}")
 
 
